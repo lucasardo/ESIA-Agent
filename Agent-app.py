b523212fe031812ae667428b2d67d425babcc975
Luca Sardo <luca.sardo14@gmail.com>
2024-05-10 18:01:18 +0200

    Update Agent-app.py

--- Agent-app.py
+++ Agent-app.py
@@ -149,37 +149,38 @@ if init_prompt:
             else:
                 st.write(":x: Low confidence. Search score: ", score)
     
-######################################
-####### ENVIRONMENTAL ENGINEER
-######################################
-                
-    st.markdown('#')
-    session_id = session_id + 1
-    st.write("<h2 style='color: #F9423A;'>ENVIRONMENTAL IMPACT", unsafe_allow_html=True)
-    
-    @st.cache(suppress_st_warning=True)
-    def cached_env_chapter(question, index_name, session_id):
-        return generate_env_chapter(question, index_name, session_id)      
-    
-    response_env = cached_env_chapter(question, index_name, session_id)
-    
-    st.markdown(response_env)
+### DOWNLOAD WORD DOCUMENT
+    def save_as_word_small(response_1):
+        doc = Docx()
+        
+        def add_content_with_headings(content):
+            lines = content.split('\n')
+            for line in lines:
+                if line.startswith('####'):
+                    heading = doc.add_heading(line.lstrip('# '), level=2)
+                    for run in heading.runs:
+                        run.font.color.rgb = RGBColor(255, 0, 0)
+                elif line.startswith('###'):
+                    heading = doc.add_heading(line.lstrip('# '), level=1)
+                    for run in heading.runs:
+                        run.font.color.rgb = RGBColor(255, 0, 0)
+                else:
+                    doc.add_paragraph(line)
+            doc.add_paragraph()
 
-    ### RETRIEVE CITANTIONS AND RRF SCORES
-    search_results = simple_hybrid_search(question, index_name, filter, search_url, search_credential, azure_endpoint, openai_api_key, openai_api_version, embedding_deployment_name)
-    sources_nodes = list_sources_nodes(search_results)
- 
-    with st.expander("See sources"):
-        for node in sources_nodes[0:5]:
-            file_name = os.path.basename(node["path"])
-            st.write("Source file: ", file_name)
-            raw_score = node["score"]
-            score = "{:.2f}".format(raw_score)
-            score = float(score)
-            if score >= 0.02:
-                st.write(":heavy_check_mark: High confidence. Search score: ", score)
-            elif score >= 0.01:
-                st.write(":warning: Medium confidence. Search score: ", score)
-            else:
-                st.write(":x: Low confidence. Search score: ", score)
+        # Add markdown to the document with headings
+        add_content_with_headings(response_1)
 
+        # Save the document
+        doc.save("ESIA Draft.docx")
+    
+    save_as_word_small(response_intro)
+    
+    with open("ESIA Draft.docx", "rb") as f:
+        bytes_data = f.read()
+    st.download_button(
+        label="Click here to download",
+        data=bytes_data,
+        file_name="ESIA Draft.docx",
+        mime="application/docx"
+    )
