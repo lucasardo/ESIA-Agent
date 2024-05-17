58272f5c26b06cb11c6eb90748099fee0a4ea9ac
Luca Sardo <luca.sardo14@gmail.com>
2024-05-17 11:55:15 +0200

    Update Agent-app.py

--- Agent-app.py
+++ Agent-app.py
@@ -227,7 +227,10 @@ if init_prompt:
     
     chain = CONCLUSION_PROMPT | llm | output_parser
       
-    response_conclusion = chain.invoke({"summary_question": "Write a conclusion chapter for an Environmental and Social Impact Assessment, using information provided in the message history."})
+    response_conclusion = chain.invoke(
+        {"summary_question": "Write a conclusion chapter for an Environmental and Social Impact Assessment, using information provided in the message history."},
+        config={"configurable": {"session_id": session_id}}
+        )
     
     st.markdown(response_conclusion)
 
